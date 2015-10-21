f78862f41c74b46da6186137997bc113f0c27601
xxx111x1 <minzou@live.com>
2015-10-21 21:39:55 +0800

    add wechat project

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Min
+ * Date: 2015/10/21
+ * Time: 21:33
+ */
+include "wx_sample.php";
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->valid();
+?>
