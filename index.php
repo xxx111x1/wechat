85b8fa1e38b2655578a71bf31f2e61e4f4b06e9b
xxx111x1 <minzou@live.com>
2015-10-21 22:14:43 +0800

    add wechat project

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
  * Time: 21:33
  */
 include "wx_sample.php";
-echo "Hell World";
-file_put_contents("/var/www/html/wechat/log.txt", "accessed",FILE_APPEND);
 $wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
-file_put_contents("log.txt", "accessed",FILE_APPEND);
+if (isset($_GET['echostr'])) {
+    $wechatObj->valid();
+}else{
+    $wechatObj->responseMsg();
+}
 ?>
